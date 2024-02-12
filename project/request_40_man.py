8e61c6b5d9b92aa1faa3b2a397caa0975f07eab7
Julian Peterson <97069718+juliantpeterson@users.noreply.github.com>
2024-02-12 12:11:28 -0800

    Added files via upload
    Has first sprint deliverables

---
+++ project/request_40_man.py
@@ -0,0 +1,46 @@
+import requests
+import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:4040")
+
+while True:
+    # wait for message from client
+    team_string = str(socket.recv()).upper()[2:-1]
+    # format name for searching in list
+    name = team_string.split()[-1].lower().capitalize()
+
+    # create url and GET request to search for team number
+    host = "http://lookup-service-prod.mlb.com"
+    path = "/json/named.team_all_season.bam?sport_code='mlb'&season='2024'"
+    url = host + path
+    response = requests.get(url)
+    teams = response.json()['team_all_season']['queryResults']['row']
+
+    for team in teams:
+        if name == team['name']:
+            team_id = team['team_id']
+
+    # create url to team roster
+    path = f"/json/named.roster_40.bam?team_id='{team_id}'"
+    url = host + path
+
+
+    # A GET request to the API to get roster
+    response = requests.get(url)
+    #print(response)
+    #print(response.content)
+
+    # Print the response
+    response_json = response.json()
+    players = response_json['roster_40']['queryResults']['row']
+    print(players)
+    socket.send_json(players)
+
+    roster_list = []
+    player_data = {}
+    for player in players:
+        print(player['name_display_first_last'])
+
